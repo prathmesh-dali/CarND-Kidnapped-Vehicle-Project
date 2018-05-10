7cc8c3aab5adb2013fcc97473958ceabfb1cc35d
prathmesh-dali <prathmesh.dali@gmail.com>
2018-05-11 00:02:45 +0530

    refactoring

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
 	num_particles = 100;
 
-	// initialize a random engine (?)
+	// initialize a random engine
 	default_random_engine gen;
 
 	// This line creates a normal (Gaussian) distribution for x.
